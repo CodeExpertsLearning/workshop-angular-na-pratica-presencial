4cafca4e4a8ea526741376f2a0f5e0f6acafae01
Nanderson Castro <nandokstro@gmail.com>
2018-06-02 12:15:14 -0300

    Adicionando HttpService e atualizando aplicacao.

--- workshop/src/app/app.component.ts
+++ workshop/src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
-import { StorageService } from './storage.service';
+import { StorageService } from './service/storage.service';
+import { HttpService } from './service/http.service';
+declare var toastr;
 
 @Component({
   selector: 'app-root',
@@ -8,44 +10,95 @@ import { StorageService } from './storage.service';
 })
 export class AppComponent {
 
-  users: Array<{name: string, email: string}>;
+//  users: Array<{name: string, email: string}>;
 
-  user: {'name': string, 'email': string} = {
+  // user: {'name': string, 'email': string} = {
+  //   'name': '',
+  //   'email': ''
+  // };
+
+  beers: Array<{ 'name', 'price', 'type', 'mark', 'img' }>;
+
+  beer: { 'name', 'price', 'type', 'mark', 'img' } = {
     'name': '',
-    'email': ''
+    'price': '',
+    'type': '',
+    'mark': '',
+    'img': ''
   };
 
-  constructor(private storage: StorageService) {
-    this.users = JSON.parse(this.storage.get('users'));
+  courses;
+  isUpdate: boolean = false;
+
+  constructor(
+    private httpService: HttpService,
+    private storage: StorageService
+  ) {
+    // this.beers = JSON.parse(this.storage.get('users'));
+
+    this.httpService.get('/beers').subscribe(res => this.beers = res);
   }
 
-  add(user: {'name': string, 'email': string}) {
-    let users;
-    users = this.storage.get('users');
+  // add(user: {'name': string, 'email': string}) {
+  //   let users;
+  //   users = this.storage.get('users');
 
-    if (users !== null) {
+  //   if (users !== null) {
 
-      users = JSON.parse(users);
-      users.push(user);
+  //     users = JSON.parse(users);
+  //     users.push(user);
 
-      this.storage.set('users', JSON.stringify(users));
+  //     this.storage.set('users', JSON.stringify(users));
 
-    } else {
-      users = [user];
-      this.storage.set('users', JSON.stringify(users));
-    }
+  //   } else {
+  //     users = [user];
+  //     this.storage.set('users', JSON.stringify(users));
+  //   }
 
-    this.users = users;
+  //   this.users = users;
+  // }
+
+  save(beer) {
+    this.httpService.post('/beers', beer).subscribe(res => toastr.success('Sucesso', res.msg));
   }
 
-  remove(user) {
-    let users = JSON.parse(this.storage.get('users'));
-    users = users.filter(function(u){
-       return u.email != user.email;
+  edit(beer) {
+    this.isUpdate = true;
+    this.beer = beer;
+  }
+
+  update(beer) {
+    this.httpService.put('/beers', beer).subscribe(res => {
+
+      this.beer = {
+        'name': '',
+        'price': '',
+        'type': '',
+        'mark': '',
+        'img': ''
+      };
+      this.isUpdate = false;
+
+      toastr.success('Sucesso', res.msg);
+      window.location.reload();
     });
-
-    this.storage.set('users', JSON.stringify(users));
-
-    this.users = users;
   }
+
+  remove(beer) {
+    this.httpService.delete(`/beers/${beer.id}`).subscribe(res => {
+      toastr.success('Sucesso', res.msg);
+      window.location.reload();
+    });
+  }
+
+  // remove(user) {
+  //   let users = JSON.parse(this.storage.get('users'));
+  //   users = users.filter(function(u){
+  //      return u.email != user.email;
+  //   });
+
+  //   this.storage.set('users', JSON.stringify(users));
+
+  //   this.users = users;
+  // }
 }
---
+++ workshop/src/app/service/http.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HttpService {
+
+  private url = 'http://192.168.0.13:8080/v1';
+
+  constructor(private http: HttpClient) { }
+
+  get(endpoint) {
+    return this.http.get<any[]>(`${this.url}${endpoint}`);
+  }
+
+  post(endpoint, data) {
+    return this.http.post<{msg}>(`${this.url}${endpoint}`, data);
+  }
+
+  put(endpoint, data) {
+    return this.http.put<{msg}>(`${this.url}${endpoint}`, data);
+  }
+
+  delete(endpoint) {
+    return this.http.delete<{msg}>(`${this.url}${endpoint}`);
+  }
+}
